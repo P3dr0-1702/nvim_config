6a629cf5e47435b6c42f391fede0cfb69e1892f0
P3dr0-1702 <supermegagamerprofissional@gmail.com>
2025-11-16 18:32:46 +0000

    completely revanped configs, LSP is updated to neovim 0.11+

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,100 @@
+-- Leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local funcs = require("core.funcs")
+
+vim.keymap.set("n", "<leader>b", ":bd<CR>", { silent = true, noremap = true })
+--   funcs.safe_bdelete()
+-- end, {})
+-- vim.api.nvim_create_user_command("WQ", function()
+--   funcs.save_and_close()
+-- end, {})
+--
+-- vim.cmd([[
+--     cnoreabbrev <expr> q getcmdtype() == ':' && getcmdline() == 'q' ? 'Q' : 'q'
+--     cnoreabbrev <expr> wq getcmdtype() == ':' && getcmdline() == 'wq' ? 'WQ' : 'wq'
+-- ]])
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if package.loaded["toggleterm"] then
+      vim.keymap.set(
+        "n",
+        "<leader><leader>",
+        "<cmd>ToggleTerm<CR>",
+        { desc = "Toggle terminal", noremap = true, silent = true }
+      )
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if package.loaded["telescope.builtin"] then
+      local builtin = require("telescope.builtin")
+      -- Buffer search
+      vim.keymap.set("n", "<leader>p", builtin.buffers, { desc = "Find existing buffers" })
+
+      -- Keep other telescope mappings here
+      vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+      vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+    end
+  end,
+})
+
+-- NvimTree toggle
+vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>")
+
+-- 42 formatter
+vim.keymap.set("n", "<leader>i", function()
+  vim.cmd("silent! write")
+  vim.cmd("!c_formatter_42 %")
+  vim.cmd("edit!")
+end, { noremap = true, silent = true, desc = "Format with 42 formatter" })
+
+-- Search
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Terminal mode exit
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Window navigation
+vim.keymap.set("n", "<leader>h", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<leader>l", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<leader>j", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<leader>k", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+--cut command
+vim.keymap.set({ "n", "v" }, "<leader>x", '"+d', { desc = "Cut to clipboard" })
+-- Buffer navigation
+vim.keymap.set("n", "<C-l>", ":BufferLineCycleNext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<C-h>", ":BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+
+-- Make delete operations not affect the yank register
+vim.keymap.set("n", "dd", '"_dd', { noremap = true, desc = "Delete line without yanking" })
+vim.keymap.set("n", "d", '"_d', { noremap = true, desc = "Delete without yanking" })
+vim.keymap.set("v", "d", '"_d', { noremap = true, desc = "Delete without yanking" })
+vim.keymap.set("n", "D", '"_D', { noremap = true, desc = "Delete to end of line without yanking" })
+vim.keymap.set("n", "x", '"_x', { noremap = true, desc = "Delete character without yanking" })
+vim.keymap.set("v", "x", '"_x', { noremap = true, desc = "Delete character without yanking" })
+vim.keymap.set("n", "c", '"_c', { noremap = true, desc = "Change without yanking" })
+vim.keymap.set("v", "c", '"_c', { noremap = true, desc = "Change without yanking" })
+vim.keymap.set("n", "C", '"_C', { noremap = true, desc = "Change to end of line without yanking" })
+
+local M = {}
+
+M.visual_multi = {
+  maps = {
+    ["Find Under"] = "<C-d>", -- select next occurrence
+    ["Find Subword Under"] = "<C-d>", -- select next subword
+    ["Select All"] = "<C-a>", -- select all occurrences
+    ["Add Cursor Up"] = "<C-Up>", -- add cursor above
+    ["Add Cursor Down"] = "<C-Down>", -- add cursor below
+  },
+  highlight = "visual", -- optional, default is 'visual'
+}
+return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,21 +1,4 @@
--- lua/plugins/init.lua
-local plugin_files = {
-  "42_header", "toggle_term", "dev_webicons", "gitsigns",
-  "whichkey", "telescope", "mini_pairs", "buffer_line",
-  "conform", "lazydev", "todo-comment", "lsp",
-  "treesitter", "completion", "project_helper",
-  "kanagawa", "vim_mmulti_cursor",
+return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
 }
-
-local plugins = {}
-
-for _, name in ipairs(plugin_files) do
-  local ok, tbl = pcall(require, "plugins.plugins." .. name)
-  if ok and tbl then
-    for _, plugin in ipairs(tbl) do
-      table.insert(plugins, plugin)
-    end
-  end
-end
-
-return plugins
