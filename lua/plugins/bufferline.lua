8a8f9a58af58a4e2925d841b10019c18459371e8
P3dr0-1702 <supermegagamerprofissional@gmail.com>
2026-02-11 10:21:01 +0000

    stabe version 20260211

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,6 +5,22 @@ return {
   opts = {
     options = {
       mode = "buffers",
+      groups = {
+        items = {
+          {
+            name = "outside_cwd",
+            highlight = { fg = "#ff9e64", bg = "#2a1f1a", bold = true },
+            matcher = function(buf)
+            if not buf.path or buf.path == "" then
+              return false
+              end
+              local cwd = vim.loop.fs_realpath(vim.fn.getcwd()) or vim.fn.getcwd()
+              local path = vim.loop.fs_realpath(buf.path) or buf.path
+              return not vim.startswith(path, cwd .. "/")
+              end,
+          },
+        },
+      },
     },
   },
 }
